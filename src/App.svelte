ca00e86a0e7db3e82c608499788487e2b62e86d8
FSGabrsek <f.gabrsek@gmail.com>
2022-02-08 17:01:05 +0100

    added wordle global styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	export let name;
+	
 </script>
 
 <main>
@@ -7,14 +7,5 @@
 </main>
 
 <style>
-	:global(body) {
-		margin: 0;
-		padding: 0;
-	}
-
-	main {
-		width: 100%;
-		height: 100%;
-		background-color: gray;
-	}
+	
 </style>
