7d12f9ddd239524fcb076ef7eebf9df44c84f035
FSGabrsek <f.gabrsek@gmail.com>
2022-02-08 19:23:22 +0100

    added header framework

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script>
-	
+	import Header from './Header.svelte';
 </script>
 
 <main>
-	
+	<Header id="header"/>
 </main>
 
 <style>
-	
+	main {
+		position: absolute;
+		left: 50%;
+		transform: translate(-50%, 0);
+		
+		max-width: var(--game-max-width);
+		width: 100%;
+	}
 </style>
