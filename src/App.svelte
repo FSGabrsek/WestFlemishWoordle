8b4e03d1a76b0b193adfc7345c46a39f74a9762b
FSGabrsek <f.gabrsek@gmail.com>
2022-02-08 11:06:27 +0100

    svelte setup

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	
+</main>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	main {
+		width: 100%;
+		height: 100%;
+		background-color: gray;
+	}
+</style>
