1c3b96b442737079c0aec9601a76905c48159306
FSGabrsek <f.gabrsek@gmail.com>
2022-02-09 16:36:38 +0100

    fragmented icon button

---
+++ src/Header/IconButton.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let path
+
+    export let onClick;
+</script>
+
+<button on:click="{onClick}">
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+        <path fill="var(--color-tone-3)" d={path} />
+    </svg>
+</button>
+
+<style>
+    button {
+        border: none;
+        background: none;
+
+        cursor: pointer;
+    }
+
+    svg {
+        padding-inline: 4px;
+    }
+
+    *:not(svg),
+    *:not(foreignObject) > svg {
+        transform-origin: 0 0;
+    }
+</style>
